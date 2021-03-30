2254e2f8f92b301d7b218cdc2059b51245d6e5b7
Liz <liz@Liz-s-Mac.local>
2021-03-30 15:39:24 -0500

    add about page content and structure divs

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -2,8 +2,23 @@ import React from 'react';
 
 export default function About() {
   return (
-    <>
-      <h1>About Me</h1>
-    </>
+    <div id="about-page" className="page">
+      <div id="intro">
+        <h1>I'm Liz,</h1>
+        <p>a software developer</p>
+      </div>
+      <div id="headshot">
+        <img src="" alt="" />
+      </div>
+      <div id="about-me-blurb">
+        <p>
+          I take on side projects like it's my job. I have experience creating
+          graphics, logos, wireframes and websites- I'm all about the visuals. I
+          am an athlete, avid adventurer, feminist and a lifelong learner.
+          Coding gives me the opportunity to express my creativty, while meeting
+          a challenge every step of the way. This is why I choose development!
+        </p>
+      </div>
+    </div>
   );
 }
