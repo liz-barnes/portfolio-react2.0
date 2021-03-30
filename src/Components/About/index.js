6173e9de6201cfd542b720cfa43b543bbd796cd1
Liz <liz@Liz-s-Mac.local>
2021-03-30 16:01:59 -0500

    style about page using gird css

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -7,18 +7,19 @@ export default function About() {
         <h1>I'm Liz,</h1>
         <p>a software developer</p>
       </div>
-      <div id="headshot">
-        <img src="" alt="" />
-      </div>
-      <div id="about-me-blurb">
-        <p>
-          I take on side projects like it's my job. I have experience creating
-          graphics, logos, wireframes and websites- I'm all about the visuals. I
-          am an athlete, avid adventurer, feminist and a lifelong learner.
-          Coding gives me the opportunity to express my creativty, while meeting
-          a challenge every step of the way. This is why I choose development!
-        </p>
-      </div>
+          <img
+            id="headshot"
+            src="https://peterhurley.com/sites/default/files/styles/big/public/photos/2015/02/37/kearrancpeterhurley.jpg?itok=PZbogs-G"
+            alt=""
+          />
+          <p id="about-me-blurb">
+            I take on side projects like it's my job. I have experience creating
+            graphics, logos, wireframes and websites- I'm all about the visuals.
+            I am an athlete, avid adventurer, feminist and a lifelong learner.
+            Coding gives me the opportunity to express my creativty, while
+            meeting a challenge every step of the way. This is why I choose
+            development!
+          </p>
     </div>
   );
 }
