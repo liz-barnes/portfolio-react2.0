00ac009f70094dc04ebb10f90ec9fb819b9bcb61
Liz <liz@Liz-s-Mac.local>
2021-04-01 21:16:39 -0500

    add spacing and styling - hover, color, font weight

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -1,12 +1,12 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default function Nav() {
   return (
     <nav className='navbar'>
-      <Link to='/' className='navbar-item'>about</Link>
-      <Link to='/work' className='navbar-item'>work</Link>
-      <Link to='/resume' className='navbar-item'>resume</Link>
-      <Link to='/contact' className='navbar-item'>contact</Link>
+      <NavLink to='/' className='navbar-item'>about</NavLink>
+      <NavLink to='/work' className='navbar-item'>work</NavLink>
+      <NavLink to='/resume' className='navbar-item'>resume</NavLink>
+      <NavLink to='/contact' className='navbar-item'>contact</NavLink>
     </nav>
   );
 }
