2bb7620c184b24b9052e0503459447217d292332
Liz <liz@Liz-s-Mac.local>
2021-06-25 15:21:18 -0500

    update about blurb

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -8,19 +8,14 @@ export default function About() {
         <h1 className="heading">I'm Liz,</h1>
         <p className="title">a software developer</p>
       </div>
-          <img
-            id="headshot"
-            src={headshot}
-            alt=""
-          />
-          <p id="about-me-blurb">
-            I take on side projects like it's my job. I have experience creating
-            graphics, logos, wireframes and websites- I'm all about the visuals.
-            I am an athlete, avid adventurer, feminist and a lifelong learner.
-            Coding gives me the opportunity to express my creativty, while
-            meeting a challenge every step of the way. This is why I choose
-            development!
-          </p>
+      <img id="headshot" src={headshot} alt="" />
+      <p id="about-me-blurb">
+        My passion first fixated on design while making graphics for my sports
+        team in 2018. Since then I have explored and been completely captivated
+        by UX/UI and web development. Coding gives me the opportunity to express
+        my creativity, while meeting a challenge every step of the way. This is
+        why I choose development!
+      </p>
     </div>
   );
 }
