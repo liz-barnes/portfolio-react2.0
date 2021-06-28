960e4af15b8f0d5feeb9cad71aad5194b6f2f355
Liz <liz@Liz-s-Mac.local>
2021-06-27 19:34:49 -0500

    update resume blurb

--- src/Components/Resume/index.js
+++ src/Components/Resume/index.js
@@ -7,7 +7,7 @@ import resume from '../../helpers/images/liz_barnes_resume.pdf';
 export default function Resume() {
   return (
     <div className="page" id="resume-page">
-      <h3 className="outro">Thank you for visiting my site and checking out my work! Please reach out! I would love to collaborate, talk code or anything in between.</h3>
+      <h3 className="outro">Thank you for visiting my site and checking out my work! Please reach out –– I would love to collaborate, talk code or anything in between.</h3>
       <div className="pdf-button-container">
         <a className="pdf-link" href={resume} target="_blank" rel="noreferrer">
             Click here to view/download my resume!
